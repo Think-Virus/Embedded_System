47a749d383a02b1e6294b30c7f6478c0b6bcd8af
think_virus <chuncccc1004@gmail.com>
2025-02-27 16:26:26 -0600

    Feat(script_NUCLEO): Receive data

--- serial_device.py
+++ serial_device.py
@@ -4,16 +4,25 @@ import serial.tools.list_ports
 def get_ports():
     port_number = None
     ports = list(serial.tools.list_ports.comports())
-    for port in ports:
-        # Test1
-        print(port.manufacturer)
 
+    for port in ports:
         if port.manufacturer.startswith('STM'):
             port_number = port.device
 
-        # Test2
-        # print(port_number)
-
     return port_number
 
-print(get_ports())
+
+def receive_data(iot_device):
+    try:
+        value = iot_device.readline()
+        print(value)
+    except serial.SerialException as e:
+        print(e)
+
+
+if __name__ == "__main__":
+    port_number = get_ports()
+    iot_device = serial.Serial(port=port_number, baudrate=115200, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, timeout=1)
+
+    while True:
+        receive_data(iot_device)
